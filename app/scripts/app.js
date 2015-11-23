fabdb5ba31ca6a89258793b195932044abc2cfef
Meza <mezad@Administrators-MacBook-Air.local>
2015-11-23 13:18:00 -0500

    set up initial skeleton

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name App
+ * @description
+ * # App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('App', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the App
+ */
+angular.module('App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
