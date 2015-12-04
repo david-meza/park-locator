485bd6ce3cd2ca4b4596bff69ed32b75026dac85
David Meza <davidmeza@outlook.com>
2015-12-03 23:55:25 -0500

    better icons added\!

--- app/scripts/services/httpInterceptor.js
+++ app/scripts/services/httpInterceptor.js
@@ -7,17 +7,17 @@ angular.module('parkLocator').factory('httpInterceptor', ['$q', '$rootScope',
 
   return {
     request: function (config) {
-      if(++loadingCount === 1) { $rootScope.$broadcast('loading:progress'); }
+      if(++loadingCount >= 1) { $rootScope.$broadcast('loading:progress'); }
       return config || $q.when(config);
     },
 
     response: function (response) {
-      if(--loadingCount === 0) { $rootScope.$broadcast('loading:finish'); }
+      if(loadingCount <= 0) { $rootScope.$broadcast('loading:finish'); }
       return response || $q.when(response);
     },
 
     responseError: function (response) {
-      if(--loadingCount === 0) { $rootScope.$broadcast('loading:finish'); }
+      if(loadingCount <= 0) { $rootScope.$broadcast('loading:finish'); }
       return $q.reject(response);
     }
   };
