2b13821b69f61d99555731919c63ee36b7ddf802
David Meza <davidmeza@outlook.com>
2016-01-14 13:11:53 -0500

    add wake county parks source

--- app/scripts/services/parkService.js
+++ app/scripts/services/parkService.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('parkLocator').factory('parkService', ['$http', '$state', 'uiGmapGoogleMapApi',
-	function ($http, $state, gMapsApi) {
+angular.module('parkLocator').factory('parkService', ['$http', '$state', 'uiGmapGoogleMapApi', '$q',
+	function ($http, $state, gMapsApi, $q) {
 	
   var mapsApi;
 
@@ -53,93 +53,84 @@ angular.module('parkLocator').factory('parkService', ['$http', '$state', 'uiGmap
   };
 
 
-  var _generateMarkers = function (response) {
+  var _generateMarkers = function (parks) {
 
-    if (typeof response.data === 'object') {
+    // Empty the existing parks array before adding the new results
+    markers.content.splice(0, markers.content.length);
 
-      // Empty the existing parks array before adding the new results
-      if (markers.content.length > 0) {
-        markers.content.splice(0, markers.content.length);
-      }
+    angular.forEach(parks, function (park) {
+      var p = park.attributes;
+      var parkName = p.NAME.replace(/\W+/g, '').toLowerCase();
+      var marker = {
+        id: p.OBJECTID,
+        name: p.NAME,
+        address: p.ADDRESS,
+        url: p.URL,
+        phone: p.PHONE,
+        alias1: p.ALIAS1,
+        alias2: p.ALIAS2,
+        scale: p.SCALE,
+        artscenter: p.ARTSCENTER === 'Yes',
+        ballfields: p.BALLFIELDS === 'Yes',
+        boatrentals: p.BOATRENTALS === 'Yes',
+        bocce: p.BOCCE === 'Yes',
+        bmxtrack: p.BMXTRACK === 'Yes',
+        canoe: p.CANOE === 'Yes',
+        canoelaunch: p.CANOE === 'Yes',
+        carousel: p.CAROUSEL === 'Yes',
+        discgolf: p.DISCGOLF === 'Yes',
+        dogpark: p.DOGPARK === 'Yes',
+        envctr: p.ENVCTR === 'Yes',
+        fishing: p.FISHING === 'Yes',
+        greenwayaccess: p.GREENWAYACCESS === 'Yes',
+        gym: p.GYM === 'Yes',
+        horseshoe: p.HORSESHOE === 'Yes',
+        inlineskating: p.INLINESKATING === 'Yes',
+        boatride: p.BOATRIDE === 'Yes',
+        library: p.LIBRARY === 'Yes',
+        amusementtrain: p.AMUSEMENTTRAIN === 'Yes',
+        multipurposefield: p.MULTIPURPOSEFIELD === 'Yes',
+        outdoorbasketball: p.OUTDOORBASKETBALL === 'Yes',
+        picnicshelter: p.PICNICSHELTER === 'Yes',
+        playground: p.PLAYGROUND === 'Yes',
+        pool: p.POOL === 'Yes',
+        communitycenter: p.COMMUNITYCENTER === 'Yes',
+        neighborhoodcenter: p.NEIGHBORHOODCENTER === 'Yes',
+        sandvolleyball: p.SANDVOLLEYBALL === 'Yes',
+        skatepark: p.SKATEPARK === 'Yes',
+        tenniscenter: p.TENNISCENTER === 'Yes',
+        tenniscourts: p.TENNISCOURTS === 'Yes',
+        theater: p.THEATER === 'Yes',
+        track: p.TRACK === 'Yes',
+        walkingtrails: p.WALKINGTRAILS === 'Yes',
+        uniquesp: p.UNIQUESP === 'Yes',
+        handball: p.HANDBALL === 'Yes',
+        active_adult: p.ACTIVE_ADULT === 'Yes',
+        teen: p.TEEN === 'Yes',
+        museum: p.MUSEUM === 'Yes',
+        
+        icon: 'https://s3.amazonaws.com/davidmeza/Park_Locator/tree-small.png',
+        latitude: park.geometry.y,
+        longitude: park.geometry.x,
 
-      response.data.features.forEach(function(park){
-        var p = park.attributes;
-        var marker = {
-          id: p.OBJECTID,
-          name: p.NAME,
-          address: p.ADDRESS,
-          url: p.URL,
-          phone: p.PHONE,
-          alias1: p.ALIAS1,
-          alias2: p.ALIAS2,
-          scale: p.SCALE,
-          artscenter: p.ARTSCENTER === 'Yes',
-          ballfields: p.BALLFIELDS === 'Yes',
-          boatrentals: p.BOATRENTALS === 'Yes',
-          bocce: p.BOCCE === 'Yes',
-          bmxtrack: p.BMXTRACK === 'Yes',
-          canoe: p.CANOE === 'Yes',
-          canoelaunch: p.CANOE === 'Yes',
-          carousel: p.CAROUSEL === 'Yes',
-          discgolf: p.DISCGOLF === 'Yes',
-          dogpark: p.DOGPARK === 'Yes',
-          envctr: p.ENVCTR === 'Yes',
-          fishing: p.FISHING === 'Yes',
-          greenwayaccess: p.GREENWAYACCESS === 'Yes',
-          gym: p.GYM === 'Yes',
-          horseshoe: p.HORSESHOE === 'Yes',
-          inlineskating: p.INLINESKATING === 'Yes',
-          boatride: p.BOATRIDE === 'Yes',
-          library: p.LIBRARY === 'Yes',
-          amusementtrain: p.AMUSEMENTTRAIN === 'Yes',
-          multipurposefield: p.MULTIPURPOSEFIELD === 'Yes',
-          outdoorbasketball: p.OUTDOORBASKETBALL === 'Yes',
-          picnicshelter: p.PICNICSHELTER === 'Yes',
-          playground: p.PLAYGROUND === 'Yes',
-          pool: p.POOL === 'Yes',
-          communitycenter: p.COMMUNITYCENTER === 'Yes',
-          neighborhoodcenter: p.NEIGHBORHOODCENTER === 'Yes',
-          sandvolleyball: p.SANDVOLLEYBALL === 'Yes',
-          skatepark: p.SKATEPARK === 'Yes',
-          tenniscenter: p.TENNISCENTER === 'Yes',
-          tenniscourts: p.TENNISCOURTS === 'Yes',
-          theater: p.THEATER === 'Yes',
-          track: p.TRACK === 'Yes',
-          walkingtrails: p.WALKINGTRAILS === 'Yes',
-          uniquesp: p.UNIQUESP === 'Yes',
-          handball: p.HANDBALL === 'Yes',
-          active_adult: p.ACTIVE_ADULT === 'Yes',
-          teen: p.TEEN === 'Yes',
-          museum: p.MUSEUM === 'Yes',
-          
-          icon: 'https://s3.amazonaws.com/davidmeza/Park_Locator/tree-small.png',
-          latitude: park.geometry.y,
-          longitude: park.geometry.x,
+        markerClick: _markerClick,
+        options: {
+          title: p.NAME,
+          labelAnchor: '0 0',
+          animation: (mapsApi ? mapsApi.Animation.DROP : 2)
+        },
+      };
 
-          markerClick: _markerClick,
-          options: {
-            title: p.NAME,
-            labelAnchor: '0 0',
-            animation: (mapsApi ? mapsApi.Animation.DROP : 2)
-          },
-        };
+      // Storing parks both individually as key on markers object and as an array of parks
+      if (!markers[parkName]) { markers[parkName] = marker; }
+      markers.content.push(marker);
+    });
 
-        // Storing parks both individually as key on markers object and as an array of parks
-        var parkName = p.NAME.replace(/\W+/g, '').toLowerCase();
-        if (!markers[parkName]) { markers[parkName] = marker; }
-
-        markers.content.push(marker);
-      });
-      
-      // if (markers.content.length > 0) { $state.go('home.park', { 'name': markers.content[0].name.replace(/\W+/g, '').toLowerCase() }); }
-
-    } else {
-      console.log('error', response);
-    }
 	};
 
 	var _logAjaxError = function (error) {
 		console.log(error);
+    return $q.reject(error);
 	};
 
   var updateParkMarkers = function (selectedActivities) {
@@ -151,19 +142,51 @@ angular.module('parkLocator').factory('parkService', ['$http', '$state', 'uiGmap
       if (idx <= selectedActivities.length - 2) { query += '+AND+'; }
       
     });
-    getParksInfo(query);
+    
+    var promise = $q.all([ _getParksInfo(query), _getWakeParks(query) ]).then(_removeParkDupes, _logAjaxError);
   };
 
-  var getParksInfo = function (where) {
-    var url = 'https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=' + (where ? where : '1%3D1') + '&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson';
-  	$http({
+  var _getParksInfo = function (where) {
+    var url = 'https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0/query?where=' + (where || '1%3D1') + '&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson';
+    
+    return $http({
+      method: 'GET',
+      url: url
+    });
+  };
+
+  var _removeParkDupes = function (responsesArr) { 
+    var uniques = [],
+        mapped = {};
+
+    angular.forEach(responsesArr, function (response) {
+
+      angular.forEach(response.data.features, function (park) {
+        if (!mapped[park.attributes.NAME]) {
+          mapped[park.attributes.NAME] = park;
+          uniques.push(park);
+        }
+      });
+
+    });
+
+    // console.log(uniques);
+
+    return $q.resolve(uniques, _generateMarkers);
+
+  };
+
+  var _getWakeParks = function (query) {
+  	var url = 'http://maps.wakegov.com/arcgis/rest/services/Test/ParksinWake/MapServer/0/query?where=' + (query ? ('JURISDICTION+<>+%27City+of+Raleigh%27+AND+' + query) : 'JURISDICTION+<>+%27City+of+Raleigh%27') + '&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson'
+
+    return $http({
   		method: 'GET',
   		url: url
-  	}).then(_generateMarkers, _logAjaxError);
+  	});
   };
 
   // This gets automatically called because of the $watchCollection in MapCtrl
-  // getParksInfo();
+  // _getParksInfo();
 
 
 	return {
