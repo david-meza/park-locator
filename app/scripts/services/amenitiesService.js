994074ed1fec478283aa353e4e294d6fa5eaaadd
David Meza <davidmeza@outlook.com>
2016-02-19 09:49:29 -0500

    generate the desired json response to handle park activities data

--- app/scripts/services/amenitiesService.js
+++ app/scripts/services/amenitiesService.js
@@ -3,7 +3,7 @@
 angular.module('parkLocator').factory('amenitiesService', ['$http', '$q',
 	function($http, $q){
 	
-	var list = { categories: {}, uniques: [], activitiesPos: { markers: [] } };
+	var list = { categories: { idReference: {} }, activitiesPos: { markers: [] } };
 	var selectedActivities = { current: [] };
 
 	list.activitiesPos.markersConfig = {
@@ -78,19 +78,39 @@ angular.module('parkLocator').factory('amenitiesService', ['$http', '$q',
 	var generateCategories = function (response) {
 
 		if (response.status === 200) {
-			angular.extend(list.categories, response.data);
-			angular.forEach(response.data, function (category) {
-				list[category.id] = category;
+			angular.extend(list.categories, response.data.categories);
+			angular.forEach(response.data.idReferences, function (categoryIds, categoryName) {
+				angular.forEach(categoryIds, function (id) {
+					list[id] = list.categories[categoryName];
+				});
 			});
-			list[1301] = list.categories.Swimming;
-			delete list.categories.Library;
-			delete list.categories.Softball;
-			delete list.categories['Tennis Center'];
-			delete list.categories['Youth Baseball'];
+			// delete list.categories.Library;
+			// delete list.categories.Softball;
+			// delete list.categories['Tennis Center'];
+			// delete list.categories['Youth Baseball'];
+			console.log(list);
+			return list.categories;
 		} else {
 			return logError();
 		}
 	};
+
+	var addIds = function (responses) {
+		// debugger;
+		angular.forEach([responses[1], responses[2]], function(response) {
+			angular.forEach(response.data.drawingInfo.renderer.uniqueValueInfos, function (cat) {
+				if (cat.label === 'Aquatic Center') {cat.label = 'Swimming'}
+				if (cat.label === 'Bocceball') {cat.label = 'Bocce'}
+				if (cat.label === 'Informal Playfield' || cat.label === 'Multipurpose') {cat.label = 'Multipurpose Field'}
+				if (cat.label === 'Off Leash Dog Area') {cat.label = 'Dog Park'}
+				if (cat.label === 'Track') {cat.label = 'Running'}
+				if (!list.categories[cat.label]) { return console.log(cat); }
+				list.categories.idReference[cat.label] = list.categories.idReference[cat.label] || [];
+				list.categories.idReference[cat.label].push(Number(cat.value));
+			});
+		});
+		console.log(list.categories.idReference);
+	}
 	
 
 	var getAmenitiesData = function () {
@@ -109,6 +129,22 @@ angular.module('parkLocator').factory('amenitiesService', ['$http', '$q',
 		});
 	};
 
+	var getIds = function () {
+		// First set of amenities (buildings)
+		return $http({
+			method: 'GET',
+			url: 'https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/2?f=pjson'
+		});
+	};
+
+	var getIds2 = function () {
+		// Second set of amenities (outdoors)
+		return $http({
+			method: 'GET',
+			url: 'https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/3?f=pjson'
+		});
+	};
+
 	var getJoinParkData2 = function () {
 		// Outdoor amenities join table with parks
 		return $http({
@@ -119,6 +155,7 @@ angular.module('parkLocator').factory('amenitiesService', ['$http', '$q',
 
 	var categoriesPromise = getAmenitiesData().then(generateCategories, logError);
 	$q.all([categoriesPromise, getJoinParkData(), getJoinParkData2()]).then(generateActivityMarkers, logError);
+	$q.all([categoriesPromise, getIds(), getIds2()]).then(addIds, logError);
 
 	return {
 		list: list,
