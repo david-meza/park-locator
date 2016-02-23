d032ed2a292deac4ce3cdfdaf05b0d01cebde6e0
David Meza <davidmeza@outlook.com>
2016-02-23 14:30:53 -0500

    displaying hundreds of markers is super slow

--- app/scripts/controllers/mapCtrl.js
+++ app/scripts/controllers/mapCtrl.js
@@ -14,23 +14,20 @@ angular.module('appControllers').controller('mapCtrl', ['$scope', 'mapService', 
     
   // Park Info Window
   $scope.parkWindow = parkService.parkWindow;
-  
+
   $scope.map.events.zoom_changed = function (map) {
     var z = map.getZoom();
     if ( angular.isUndefined($scope.activities.markersConfig.control.getPlurals) || angular.isUndefined($scope.parks.control.getPlurals) ) { return; }
     // Get all the activities markers, then only show them if we are zoomed in close to any particular park
     var activityMarkers = $scope.activities.markersConfig.control.getPlurals().values();
     angular.forEach(activityMarkers, function (marker) {
-      marker.gObject.setVisible(z > 14);
-      // marker.opts.visible = z > 14;
+      marker.gObject.setVisible(z >= 16);
     });
 
     var parkMarkers = $scope.parks.control.getPlurals().values();
     angular.forEach(parkMarkers, function (marker) {
-      marker.gObject.setVisible(z < 17);
-      // marker.opts.visible = z < 17;
+      marker.gObject.setVisible(z <= 16);
     });
-
     // Close info windows
     $scope.parkWindow.show = false;
   };
