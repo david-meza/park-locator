73bdd48d6ed41fb9e9dd6c28067c17d864ed6b7b
David Meza <davidmeza@outlook.com>
2016-03-09 17:20:56 -0500

    add hover event for park markers

--- app/scripts/directives/mainMap.js
+++ app/scripts/directives/mainMap.js
@@ -6,35 +6,44 @@ angular.module('appDirectives').directive('mainMap', function(){
 		controller: ['amenitiesService', '$scope', '$state', function(amenitiesService, $scope, $state) {
 
  
-      require(['esri/map', 'esri/layers/VectorTileLayer', 'esri/layers/FeatureLayer', 'esri/dijit/PopupTemplate', 'esri/dijit/LocateButton', 'esri/renderers/SimpleRenderer', 'esri/renderers/UniqueValueRenderer', 'dojo/on', 'dojo/domReady!'],
+      require(['esri/map', 'esri/layers/VectorTileLayer', 'esri/layers/FeatureLayer', 'esri/dijit/PopupTemplate', 'esri/dijit/LocateButton', 'esri/renderers/SimpleRenderer', 'esri/renderers/UniqueValueRenderer', 'dojo/on', "dijit/TooltipDialog", "dijit/popup", 'dojo/domReady!', "esri/graphic", "esri/lang",
+        "esri/Color", "dojo/number", "dojo/dom-style"],
+        function(Map, VectorTileLayer, FeatureLayer, PopupTemplate, LocateButton, SimpleRenderer, UniqueValueRenderer, on, TooltipDialog, dijitPopup, ready, Graphic, esriLang, Color, number, domStyle) {
 
-        function(Map, VectorTileLayer, FeatureLayer, PopupTemplate, LocateButton, SimpleRenderer, UniqueValueRenderer, on) {
-
+        
+        // initialize the ESRI map
         var map = new Map('map-canvas', {
           center: [-78.646, 35.785],
-          zoom: 14,
+          zoom: 13,
           logo: false
         });
 
+
+        // Geolocate button
         var geoLocate = new LocateButton({
           map: map
         }, 'geolocate-button');
-        
         geoLocate.startup();
         
+
+        // Base map layer
         var tileLyr = new VectorTileLayer('https://tiles.arcgis.com/tiles/v400IkDOw1ad7Yad/arcgis/rest/services/Vector_Tile_Basemap/VectorTileServer/resources/styles/root.json');
         map.addLayer(tileLyr);
 
+        // Amenity Markers (outdoors)
         var amenities1 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/2', {
           mode: FeatureLayer.MODE_SNAPSHOT,
           outFields: ['*']
         });
 
+        // Amenity Markers (indoors)
         var amenities2 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/3', {
           mode: FeatureLayer.MODE_SNAPSHOT,
           outFields: ['*']
         });
 
+
+        // Change all the icons for the amenities
         amenitiesService.getAmenitiesIcons().then(function(response) {
           var uniqueValueInfos = response.data;
           
@@ -56,23 +65,19 @@ angular.module('appDirectives').directive('mainMap', function(){
           map.addLayer(amenities2);
         });
         
-
+        // Greenways Layers
         var greenways = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/0');
         map.addLayer(greenways);
         var greenways2 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/1');
         map.addLayer(greenways2);
 
+        // Park Markers layer
         var parks = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0', { 
           mode: FeatureLayer.MODE_SNAPSHOT,
           outFields: ['*']
         });
 
-        on(parks, 'click', function (evt) {
-          var parkName = evt.graphic.attributes.NAME.toLowerCase().replace(/\W+/g, '');
-          console.log(parkName);
-          $state.go('home.park', {name: parkName});
-        });
-
+        // Change the icon for the park marker
         var parkSymbol = new SimpleRenderer({
           type: 'simple',
           symbol: {
@@ -82,18 +87,53 @@ angular.module('appDirectives').directive('mainMap', function(){
             width: 28
           }
         });
-
         parks.setRenderer(parkSymbol);
         map.addLayer(parks);
         
+        // Park on click event
+        on(parks, 'click', function (evt) {
+          var parkName = evt.graphic.attributes.NAME.toLowerCase().replace(/\W+/g, '');
+          console.log(parkName);
+          $state.go('home.park', {name: parkName});
         });
+
+        // Show park name on hover
+        var tooltip = new TooltipDialog({
+          id: "tooltip"
+        });
+        tooltip.startup();
+
+
+        on(parks, 'mouse-over', openTooltip);
+        on(parks, 'mouse-out', closeTooltip);
+        
+        // on(amenities1, 'mouse-over', openTooltip);
+        // on(amenities1, 'mouse-out', closeTooltip);
+        
+        // on(amenities2, 'mouse-over', openTooltip);
+        // on(amenities2, 'mouse-out', closeTooltip);
+
+        function openTooltip(evt) {
+          var content = evt.graphic.attributes.NAME;
+          tooltip.setContent(content);
+          dijitPopup.open({
+            popup: tooltip,
+            x: evt.pageX,
+            y: evt.pageY
+          });
+        }
+
+        function closeTooltip(evt) {
+          dijitPopup.close(tooltip);
+        }
+
+        
+      });
     }],
 		restrict: 'E',
     template: '<div id="map-canvas"><div id="geolocate-button"></div></div>',
 		// templateUrl: 'views/directives/map.html',
 		replace: true,
-    link: function(scope, element, attrs) {
-    }
 
 	};
 });
