9df2dc3cedd8807ce7ef67c0d4fd3e8ae84768b2
David Meza <davidmeza@outlook.com>
2016-03-11 17:22:56 -0500

    create a separate service and resolve when esri modules have loaded asynchronously

--- app/scripts/controllers/mapCtrl.js
+++ app/scripts/controllers/mapCtrl.js
@@ -2,130 +2,112 @@
   
   'use strict';
 
-  angular.module('appControllers').controller('mapCtrl', ['$scope', '$state', 'amenitiesService',
-    function($scope, $state, amenitiesService){
+  angular.module('appControllers').controller('mapCtrl', ['$scope', '$state', 'amenitiesService', 'Esri',
+    function($scope, $state, amenitiesService, Esri){
 
-  	require([
-      'esri/map', 
-      'esri/layers/VectorTileLayer', 
-      'esri/layers/FeatureLayer', 
-      'esri/dijit/LocateButton', 
-      'esri/renderers/SimpleRenderer', 
-      'esri/renderers/UniqueValueRenderer', 
-      'dojo/on', 
-      'dijit/TooltipDialog', 
-      'dijit/popup'],
-      function(Map, VectorTileLayer, FeatureLayer, LocateButton, SimpleRenderer, UniqueValueRenderer, on, TooltipDialog, dijitPopup) {
-
-      
-      // initialize the ESRI map
-      var map = new Map('map-canvas', {
-        center: [-78.646, 35.785],
-        zoom: 13,
-        logo: false
-      });
-
-
-      // Geolocate button
-      var geoLocate = new LocateButton({
-        map: map
-      }, 'geolocate-button');
-      geoLocate.startup();
-      
-
-      // Base map layer
-      var tileLyr = new VectorTileLayer('https://tiles.arcgis.com/tiles/v400IkDOw1ad7Yad/arcgis/rest/services/Vector_Tile_Basemap/VectorTileServer/resources/styles/root.json');
-      map.addLayer(tileLyr);
-
-      // Amenity Markers (outdoors)
-      var amenities1 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/2', {
-        mode: FeatureLayer.MODE_SNAPSHOT,
-        outFields: ['*']
-      });
-
-      // Amenity Markers (indoors)
-      var amenities2 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/3', {
-        mode: FeatureLayer.MODE_SNAPSHOT,
-        outFields: ['*']
-      });
-
-
-      // Change all the icons for the amenities
-      amenitiesService.getAmenitiesIcons().then(function(response) {
-        var uniqueValueInfos = response.data;
+      Esri.modulesReady().then(function(modules) {
         
-        var amenities1Symbols = new UniqueValueRenderer({
-          type: 'uniqueValue',
-          field1: 'SUBCATEGORY',
-          uniqueValueInfos: uniqueValueInfos
+        // Geolocate button
+        var geoLocate = new modules.LocateButton({
+          map: modules.map
+        }, 'geolocate-button');
+        geoLocate.startup();
+        
+
+        // Base map layer
+        var tileLyr = new modules.VectorTileLayer('https://tiles.arcgis.com/tiles/v400IkDOw1ad7Yad/arcgis/rest/services/Vector_Tile_Basemap/VectorTileServer/resources/styles/root.json');
+        modules.map.addLayer(tileLyr);
+
+        // Amenity Markers (outdoors)
+        var amenities1 = new modules.FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/2', {
+          mode: modules.FeatureLayer.MODE_SNAPSHOT,
+          outFields: ['*']
         });
-        var amenities2Symbols = new UniqueValueRenderer({
-          type: 'uniqueValue',
-          field1: 'SUBCATEGORY',
-          uniqueValueInfos: uniqueValueInfos
+
+        // Amenity Markers (indoors)
+        var amenities2 = new modules.FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/3', {
+          mode: modules.FeatureLayer.MODE_SNAPSHOT,
+          outFields: ['*']
         });
 
-        amenities1.setRenderer(amenities1Symbols);
-        amenities2.setRenderer(amenities2Symbols);
 
-        map.addLayer(amenities1);
-        map.addLayer(amenities2);
-      });
-      
-      // Greenways Layers
-      var greenways = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/0');
-      map.addLayer(greenways);
-      var greenways2 = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/1');
-      map.addLayer(greenways2);
+        // Change all the icons for the amenities
+        amenitiesService.getAmenitiesIcons().then(function(response) {
+          var uniqueValueInfos = response.data;
+          
+          var amenities1Symbols = new modules.UniqueValueRenderer({
+            type: 'uniqueValue',
+            field1: 'SUBCATEGORY',
+            uniqueValueInfos: uniqueValueInfos
+          });
+          var amenities2Symbols = new modules.UniqueValueRenderer({
+            type: 'uniqueValue',
+            field1: 'SUBCATEGORY',
+            uniqueValueInfos: uniqueValueInfos
+          });
 
-      // Park Markers layer
-      var parks = new FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0', { 
-        mode: FeatureLayer.MODE_SNAPSHOT,
-        outFields: ['*']
-      });
+          amenities1.setRenderer(amenities1Symbols);
+          amenities2.setRenderer(amenities2Symbols);
 
-      // Change the icon for the park marker
-      var parkSymbol = new SimpleRenderer({
-        type: 'simple',
-        symbol: {
-          type: 'esriPMS',
-          url: '/img/icons/park-marker.svg',
-          height: 28,
-          width: 28
+          modules.map.addLayer(amenities1);
+          modules.map.addLayer(amenities2);
+        });
+        
+        // Greenways Layers
+        var greenways = new modules.FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/0');
+        modules.map.addLayer(greenways);
+        var greenways2 = new modules.FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/Greenway/MapServer/1');
+        modules.map.addLayer(greenways2);
+
+        // Park Markers layer
+        var parks = new modules.FeatureLayer('https://maps.raleighnc.gov/arcgis/rest/services/Parks/ParkLocator/MapServer/0', { 
+          mode: modules.FeatureLayer.MODE_SNAPSHOT,
+          outFields: ['*']
+        });
+
+        // Change the icon for the park marker
+        var parkSymbol = new modules.SimpleRenderer({
+          type: 'simple',
+          symbol: {
+            type: 'esriPMS',
+            url: '/img/icons/park-marker.svg',
+            height: 28,
+            width: 28
+          }
+        });
+        parks.setRenderer(parkSymbol);
+        modules.map.addLayer(parks);
+        
+        // Park on click event
+        modules.on(parks, 'click', function (evt) {
+          var parkName = evt.graphic.attributes.NAME.toLowerCase().replace(/\W+/g, '');
+          $state.go('home.park', {name: parkName});
+        });
+
+        // Show park name on hover
+        var tooltip = new modules.TooltipDialog({ id: "tooltip" });
+        tooltip.startup();
+
+
+        modules.on(parks, 'mouse-over', openTooltip);
+        modules.on(parks, 'mouse-out', closeTooltip);
+        
+        
+        function openTooltip(evt) {
+          var content = evt.graphic.attributes.NAME;
+          tooltip.setContent(content);
+          dijitPopup.open({
+            popup: tooltip,
+            x: evt.pageX,
+            y: evt.pageY
+          });
         }
+
+        function closeTooltip(evt) {
+          dijitPopup.close(tooltip);
+        }
+
       });
-      parks.setRenderer(parkSymbol);
-      map.addLayer(parks);
-      
-      // Park on click event
-      on(parks, 'click', function (evt) {
-        var parkName = evt.graphic.attributes.NAME.toLowerCase().replace(/\W+/g, '');
-        $state.go('home.park', {name: parkName});
-      });
-
-      // Show park name on hover
-      var tooltip = new TooltipDialog({ id: "tooltip" });
-      tooltip.startup();
-
-
-      on(parks, 'mouse-over', openTooltip);
-      on(parks, 'mouse-out', closeTooltip);
-      
-      
-      function openTooltip(evt) {
-        var content = evt.graphic.attributes.NAME;
-        tooltip.setContent(content);
-        dijitPopup.open({
-          popup: tooltip,
-          x: evt.pageX,
-          y: evt.pageY
-        });
-      }
-
-      function closeTooltip(evt) {
-        dijitPopup.close(tooltip);
-      }
-    });
 
   }]);
 
---
+++ app/scripts/services/esri.js
@@ -0,0 +1,52 @@
+(function(angular) {
+
+  'use strict';
+
+  angular.module('appServices').service('Esri', ['$q', function ($q) {
+
+    var service = this;
+
+    var deferred = $q.defer();
+    
+    service.modulesReady = function() {
+      return deferred.promise;
+    }
+
+    require([
+      'esri/map', 
+      'esri/layers/VectorTileLayer', 
+      'esri/layers/FeatureLayer', 
+      'esri/dijit/LocateButton', 
+      'esri/renderers/SimpleRenderer', 
+      'esri/renderers/UniqueValueRenderer', 
+      'dojo/on', 
+      'dijit/TooltipDialog', 
+      'dijit/popup'],
+      function(Map,b,c,d,e,f,g,h,i) {
+        
+        // initialize the ESRI map
+        service.map = new Map('map-canvas', {
+          center: [-78.646, 35.785],
+          zoom: 13,
+          logo: false,
+          fadeOnZoom: true,
+          force3DTransforms: true
+        });
+
+        service.VectorTileLayer = b; 
+        service.FeatureLayer = c;
+        service.LocateButton = d;
+        service.SimpleRenderer = e;
+        service.UniqueValueRenderer = f;
+        service.on = g;
+        service.TooltipDialog = h;
+        service.dijitPopup = i;
+
+        deferred.resolve(service);
+      }
+    );
+
+
+  }]);
+
+})(window.angular);
