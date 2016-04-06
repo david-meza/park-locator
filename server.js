fbd2d8addd5206fd25c5f6974de3933ccb99d7d5
David Meza <davidmeza@outlook.com>
2016-04-06 16:07:02 -0400

    reading on express rendering

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
-
 // set the home page route
 app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('dist/index');
+  res.send('dist/index');
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+  console.log('Our app is running on http://localhost:' + port);
 });
